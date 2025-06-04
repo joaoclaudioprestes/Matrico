5e2dba2bc599949eb5bec5478c0d5cc22b4ea3f1
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2025-06-03 21:40:29 -0300

    :recycle: refactor: atualizando o relacionamento entre tabelas

--- src/main/java/com/jprestes/domain/entity/Registration.java
+++ src/main/java/com/jprestes/domain/entity/Registration.java
@@ -1,21 +1,25 @@
 package com.jprestes.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "registration")
 public class Registration {
+
     @Id
     @GeneratedValue
     private Long id;
 
-    private Long studentId;
-    private Long courseId;
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
     private String registrationDate;
     private String status; // e.g., "active", "completed", "cancelled"
 }
