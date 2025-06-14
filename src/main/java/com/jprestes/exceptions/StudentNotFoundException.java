980a67b00c861e22663f3a91c9e5b1ce0ba0864c
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2025-06-14 15:06:22 -0300

    :sparkles: feat: adicionando exceção StudentNotFoundException para tratamento de aluno não encontrado

---
+++ src/main/java/com/jprestes/exceptions/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jprestes.exceptions;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
