84c1663edb0b130ef576588f80fd256babeb4120
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2025-06-14 15:08:01 -0300

    :sparkles: feat: adicionando métodos CRUD no StudentService e melhorias em validações

--- src/main/java/com/jprestes/service/StudentService.java
+++ src/main/java/com/jprestes/service/StudentService.java
@@ -1,8 +1,12 @@
 package com.jprestes.service;
 
 import com.jprestes.domain.entity.Student;
+import com.jprestes.exceptions.MissingStudentIdException;
+import com.jprestes.exceptions.StudentNotFoundException;
 import com.jprestes.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -15,6 +19,35 @@ public class StudentService {
     public Student createStudent(Student student) {
         validateStudent(student);
         return studentRepository.save(student);
+    }
+
+    public Student updateStudent(Student student) {
+        if (student.getId() == null) {
+            throw new MissingStudentIdException("O ID do aluno é obrigatório para atualização.");
+        }
+
+        if (!studentRepository.existsById(student.getId())) {
+            throw new IllegalArgumentException("Aluno com ID: " + student.getId() + " não encontrado.");
+        }
+
+        validateStudent(student);
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void deleteStudent(Long id) {
+        if (id == null) {
+            throw new MissingStudentIdException("O ID do aluno é obrigatório para exclusão.");
+        }
+
+        if (!studentRepository.existsById(id)) {
+            throw new StudentNotFoundException("Aluno com ID: " + id + " não encontrado.");
+        }
+
+        studentRepository.deleteById(id);
     }
 
     private void validateStudent(Student student) {
