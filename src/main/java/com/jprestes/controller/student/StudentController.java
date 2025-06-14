46af2d781db4b5d8064799bd4d3778703c00b952
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2025-06-14 15:08:26 -0300

    :sparkles: feat: adicionando operações CRUD ao StudentController, validações e documentação com OpenAPI

--- src/main/java/com/jprestes/controller/student/StudentController.java
+++ src/main/java/com/jprestes/controller/student/StudentController.java
@@ -1,17 +1,21 @@
 package com.jprestes.controller.student;
 
 import com.jprestes.domain.dto.ApiResponseDTO;
-import com.jprestes.domain.dto.StudentDTO;
+import com.jprestes.domain.dto.student.StudentCreateDTO;
+import com.jprestes.domain.dto.student.StudentDTO;
+import com.jprestes.domain.dto.student.StudentUpdateDTO;
 import com.jprestes.domain.entity.Student;
 import com.jprestes.service.StudentService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Tag(name = "Alunos", description = "Operações com alunos")
 @RestController
 @RequestMapping("/alunos")
 public class StudentController {
@@ -22,15 +26,40 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<StudentDTO>> findAll() {
+        List<StudentDTO> studentDTOs = studentService.getAllStudents().stream().map(this::toDTO).collect(Collectors.toList());
+
+        return ResponseEntity.ok(studentDTOs);
+    }
+
     @PostMapping
-    public ResponseEntity<ApiResponseDTO<StudentDTO>> createStudent(@RequestBody StudentDTO studentDTO) {
-        Student created = studentService.createStudent(toEntity(studentDTO));
+    public ResponseEntity<ApiResponseDTO<StudentDTO>> createStudent(@Valid @RequestBody StudentCreateDTO createDTO) {
+        Student created = studentService.createStudent(toEntity(createDTO));
         StudentDTO createdDTO = toDTO(created);
 
         ApiResponseDTO<StudentDTO> response = new ApiResponseDTO<>(true, "Aluno criado com sucesso", createdDTO);
         return ResponseEntity.created(URI.create("/alunos/" + createdDTO.getId())).body(response);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponseDTO<StudentDTO>> updateStudent(@PathVariable Long id, @Valid @RequestBody StudentUpdateDTO updateDTO) {
+        Student updated = studentService.updateStudent(toEntity(updateDTO));
+        StudentDTO updatedDTO = toDTO(updated);
+
+        ApiResponseDTO<StudentDTO> response = new ApiResponseDTO<>(true, "Aluno atualizado com sucesso", updatedDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponseDTO<Void>> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+
+        ApiResponseDTO<Void> response = new ApiResponseDTO<>(true, "Aluno deletado com sucesso", null);
+        return ResponseEntity.ok(response);
+    }
+
+    // --- Métodos auxiliares ---
     private StudentDTO toDTO(Student student) {
         StudentDTO dto = new StudentDTO();
         dto.setId(student.getId());
@@ -39,7 +68,14 @@ public class StudentController {
         return dto;
     }
 
-    private Student toEntity(StudentDTO dto) {
+    private Student toEntity(StudentCreateDTO dto) {
+        Student entity = new Student();
+        entity.setName(dto.getName());
+        entity.setEmail(dto.getEmail());
+        return entity;
+    }
+
+    private Student toEntity(StudentUpdateDTO dto) {
         Student entity = new Student();
         entity.setName(dto.getName());
         entity.setEmail(dto.getEmail());
